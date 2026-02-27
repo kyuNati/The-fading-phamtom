62b8ac6a8790c3cc04fcc411e83a2eeb64073501
kyuNati <89148269+kyuNati@users.noreply.github.com>
2026-02-27 21:46:18 +0700

    Update Kpha.js

--- Kpha.js
+++ Kpha.js
@@ -3,15 +3,15 @@ function doKhoLa(muc){// hàm lựa chọn đọc miêu tả độ khó
  document.getElementById("cuaSo").style.display = "block";
  if(muc === "De"){
     document.getElementById("tieude").innerText = "Vùng đồng bằng";
-    document.getElementById("mota").innerText = "Chào mừng bạn đã đến với vùng đất này, ở đây an toàn nhưng khan hiếm tài nguyên.";
+    document.getElementById("mota").innerText = "Để sẵn sàng bước vào thử thách, an toàn luôn dặt lên hàng đầu. tài nguyên tuy hạn chế nhưng đồng bằng lại rất phù hợp nếu bạn không muốn một khởi đầu khó khăn.";
  }
  else if (muc === "TrungBinh") {
     document.getElementById("tieude").innerText = "Sông cạn";
-    document.getElementById("mota").innerText = "Cân bằng giữa thử thách và phần thưởng.";
+    document.getElementById("mota").innerText = "Một sự cân bằng hoàn hảo chính là sự cân bằng giữa thách thức và tài nguyên. rất có thể bạn sẽ gặp phải những điều không mong muốn nhưng hãy tin rằng phần thưởng phía trước không bạc đãi bạn đâu.";
 } 
  else{
     document.getElementById("tieude").innerText = "Vùng đá lở";
-    document.getElementById("mota").innerText = "Kẻ địch mạnh, tài nguyên hạn chế.";
+    document.getElementById("mota").innerText = "Phần thưởng lớn tương xúng với độ khó cao, một vùng đất truy cầu những nhà thám hiểm có bản lĩnh thật sự. kho báu rồng đã xuất hiện, chúng tôi tin bạn sẽ không dễ dàng bỏ qua.";
 }
 }
 // Hàm đóng cửa sổ mô tả
