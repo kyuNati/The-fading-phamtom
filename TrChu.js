17d80784c62a6963e2498080e91abeb765983096
kyuNati <89148269+kyuNati@users.noreply.github.com>
2026-02-28 23:02:32 +0700

    add Tchu css & js

---
+++ TrChu.js
@@ -0,0 +1,18 @@
+function LuuTen(){
+    let Ten = document.getElementById("DatTen").value; //.value nghĩa là lấy nội dung người dùng nhập
+    if (Ten.trim() === ""){
+        alert("Vui lòng nhập tên của bạn!"); //trim() để bỏ khoảng trắng, nếu rỗng báo lỗi
+        return;
+    }
+    document.getElementById("tenPlayer").innerText = Ten;//hiển thị tên
+    document.getElementById("CuaSoTen").style.display = "none";//ẩn cửa sổ
+    localStorage.setItem("tenPlayer", Ten);//lưu tên vào bộ nhớ trình duyệt
+}
+window.onload = function(){// Khi tải trang → kiểm tra có tên chưa
+    let LuuTen = localStorage.setItem("tenPlayer");
+    if(LuuTen){
+        document.getElementById("tenPlayer").innerText = LuuTen;
+        document.getElementById("CuaSoTen").style.display = "none";
+    }
+
+}
